57b5f739b1d900e9a607a1a5ae3622c0f48a1fa2
Benjamin Dodgson <benji@tomorrow.me>
2020-04-04 20:07:10 -0700

    removing old classes

--- Source/UI/Flows/Invite/InviteableCollectionViewController.swift
+++ Source/UI/Flows/Invite/InviteableCollectionViewController.swift
@@ -17,5 +17,4 @@ class InviteableCollectionViewController: CollectionViewController<InviteableCel
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
 }
