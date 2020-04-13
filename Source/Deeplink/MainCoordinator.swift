b7ea26a53d2c0c10a668da60c1122ed2da4ee985
Benjamin Dodgson <benji@tomorrow.me>
2020-04-12 21:16:43 -0700

    Commenting out the function

--- Source/Deeplink/MainCoordinator.swift
+++ Source/Deeplink/MainCoordinator.swift
@@ -67,14 +67,16 @@ class MainCoordinator: Coordinator<Void> {
             .withResultToast()
             .observeValue(with: { (_) in
                 self.isInitializingChat = false
+                guard let user = User.current(), user.isOnboarded else { return }
+                self.runHomeFlow()
 
-                CompleteOnboarding().makeRequest()
-                    .observe { (_) in
-                        runMain {
-                            guard let user = User.current(), user.isOnboarded else { return }
-                            self.runHomeFlow()
-                        }
-                }
+//                CompleteOnboarding().makeRequest()
+//                    .observe { (_) in
+//                        runMain {
+//                            guard let user = User.current(), user.isOnboarded else { return }
+//                            self.runHomeFlow()
+//                        }
+//                }
             })
     }
 
