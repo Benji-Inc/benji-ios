36715a1a66a9b9fe0a9f3ea6ea05bcdeef05ec44
Benjamin Dodgson <benji@tomorrow.me>
2020-05-10 12:26:20 -0700

    Updated loading animation.

--- Source/UI/Flows/Splash/SplashViewController.swift
+++ Source/UI/Flows/Splash/SplashViewController.swift
@@ -12,21 +12,32 @@ import Lottie
 class SplashViewController: FullScreenViewController {
 
     let animationView = AnimationView(name: "loading")
+    let label = SmallLabel()
     
     override func initializeViews() {
         super.initializeViews()
 
         self.view.set(backgroundColor: .background1)
 
+        self.contentContainer.addSubview(self.label)
         self.contentContainer.addSubview(self.animationView)
         self.animationView.contentMode = .scaleAspectFit
         self.animationView.loopMode = .loop
+
+        self.label.set(text: "Loading", color: .background4)
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
-        self.animationView.expandToSuperviewSize()
+        self.animationView.size = CGSize(width: 18, height: 18)
+        self.animationView.pin(.right, padding: Theme.contentOffset)
+        self.animationView.pinToSafeArea(.bottom, padding: 0)
+
+        let max = self.view.width - (Theme.contentOffset * 3) - self.animationView.width
+        self.label.setSize(withWidth: max)
+        self.label.match(.right, to: .left, of: self.animationView, offset: Theme.contentOffset * -1)
+        self.label.match(.bottom, to: .bottom, of: self.animationView)
     }
 
     override func viewWillAppear(_ animated: Bool) {
