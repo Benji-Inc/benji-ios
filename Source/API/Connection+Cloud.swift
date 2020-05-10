f3684f4a5ec296de2856774d850ea5e9732484c3
Benjamin Dodgson <benji@tomorrow.me>
2020-05-09 20:23:55 -0700

    Removing the create connection call.

--- Source/API/Connection+Cloud.swift
+++ Source/API/Connection+Cloud.swift
@@ -11,32 +11,6 @@ import Parse
 import TMROFutures
 import PhoneNumberKit
 
-struct CreateConnection: CloudFunction {
-
-    var phoneNumber: PhoneNumber
-
-    func makeRequest() -> Future<Connection> {
-        let promise = Promise<Connection>()
-
-        var params: [String: Any] = [:]
-        params["phoneNumber"] = PhoneKit.shared.format(self.phoneNumber, toType: .e164)
-
-        PFCloud.callFunction(inBackground: "createConnection",
-                             withParameters: params) { (object, error) in
-                                if let error = error {
-                                    SessionManager.shared.handleParse(error: error)
-                                    promise.reject(with: error)
-                                } else if let connection = object as? Connection {
-                                    promise.resolve(with: connection)
-                                } else {
-                                    promise.reject(with: ClientError.message(detail: "There was a problem connecting with that phone number."))
-                                }
-        }
-
-        return promise.withResultToast()
-    }
-}
-
 struct UpdateConnection: CloudFunction {
 
     var connection: Connection
