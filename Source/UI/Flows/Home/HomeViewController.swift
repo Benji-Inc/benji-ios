636b8a0967d926e42ab9c45675e3522d8915c99e
Benjamin Dodgson <benjamindodgson@gmail.com>
2020-12-24 10:36:37 -0600

    added new delegated property

--- Source/UI/Flows/Home/HomeViewController.swift
+++ Source/UI/Flows/Home/HomeViewController.swift
@@ -33,6 +33,11 @@ class HomeViewController: ViewController {
     override func initializeViews() {
         super.initializeViews()
 
+        let button = Button()
+        button.didSelect {
+            <#code#>
+        }
+
         self.current = .feed(self.feedVC)
 
         self.view.set(backgroundColor: .background1)
