ab020477297f8fba8a103d4f50cf2d901db0c38a
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-01-21 17:15:39 -0600

    loading image.

--- iOS/Flows/Channel/Channel Collection Views/Cells/PhotoMessageCell.swift
+++ iOS/Flows/Channel/Channel Collection Views/Cells/PhotoMessageCell.swift
@@ -8,10 +8,12 @@
 
 import Foundation
 import TwilioChatClient
+import SDWebImage
 
 class PhotoMessageCell: BaseMessageCell {
 
     private let imageView = DisplayableImageView()
+    private var cachedURL: URL?
 
     override func initializeViews() {
         super.initializeViews()
@@ -39,10 +41,36 @@ class PhotoMessageCell: BaseMessageCell {
     }
 
     private func loadImage(from message: TCHMessage) {
-        message.getMediaContentURL()
-            .mainSink { (urlString) in
-                self.imageView.displayable = PhotoItem(imageURLString: urlString)
-            }.store(in: &self.cancellables)
+        guard message.hasMedia() else { return }
+
+        if let url = self.cachedURL {
+            self.load(url: url)
+        } else {
+            message.getMediaContentURL()
+                .mainSink(receivedResult: { (result) in
+                    switch result {
+                    case .success(let urlString):
+                        if let url = URL(string: urlString) {
+                            self.load(url: url)
+                        }
+                    case .error(_):
+                        break
+                    }
+                }).store(in: &self.cancellables)
+        }
+    }
+
+    private func load(url: URL) {
+        self.cachedURL = url
+
+        SDWebImageManager.shared.loadImage(with: url,
+                                           options: [],
+                                           progress: { (received, expected, url) in
+                                            print("RECEIVED: \(received)")
+                                            print("EXPECTED: \(expected)")
+                                           }, completed: { (image, data, error, cacheType, finished, url) in
+                                            self.imageView.displayable = image
+                                           })
     }
 
     override func layoutContent(with attributes: ChannelCollectionViewLayoutAttributes) {
@@ -50,21 +78,10 @@ class PhotoMessageCell: BaseMessageCell {
 
         self.imageView.frame = attributes.attributes.imageFrame
     }
-}
 
-private struct PhotoItem: ImageDisplayable {
+    override func prepareForReuse() {
+        super.prepareForReuse()
 
-    var imageURLString: String
-
-    var userObjectID: String? {
-        return nil
-    }
-
-    var image: UIImage? {
-        return nil
-    }
-
-    var url: URL? {
-        return URL(string: self.imageURLString)
+        self.cachedURL = nil
     }
 }
