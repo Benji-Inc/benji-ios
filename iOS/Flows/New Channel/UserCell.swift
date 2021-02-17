b8f8d4082e35e98ac95cb4e8fd5bdb93877a91cf
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-02-17 15:31:15 -0600

    updating the user cell layout.

--- iOS/Flows/New Channel/UserCell.swift
+++ iOS/Flows/New Channel/UserCell.swift
@@ -7,17 +7,58 @@
 //
 
 import Foundation
+import TMROLocalization
+import Lottie
 
 class UserCell: CollectionViewManagerCell, ManageableCell {
     typealias ItemType = Connection
 
+    private let avatarView = AvatarView()
+    private let handleLabel = Label(font: .regularBold, textColor: .lightPurple)
+    private let nameLabel = Label(font: .small)
+    private let animationView = AnimationView(name: "checkbox")
+
+    var didTapButton: CompletionOptional = nil
+
     override func initializeSubviews() {
         super.initializeSubviews()
 
-        self.contentView.backgroundColor = .red
+        self.contentView.addSubview(self.avatarView)
+        self.contentView.addSubview(self.handleLabel)
+        self.contentView.addSubview(self.nameLabel)
+        self.contentView.addSubview(self.animationView)
     }
 
     func configure(with item: Connection) {
         guard let nonMeUser = item.nonMeUser else { return }
+
+        self.avatarView.set(avatar: nonMeUser)
+        self.handleLabel.setText(nonMeUser.handle)
+        self.nameLabel.setText(nonMeUser.fullName)
+    }
+
+    override func update(isSelected: Bool) {
+        let progress: AnimationProgressTime = isSelected ? 1.0 : 0.0
+        self.animationView.play(toProgress: progress)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        self.avatarView.setSize(for: self.contentView.height - Theme.contentOffset.doubled)
+        self.avatarView.pin(.left, padding: Theme.contentOffset)
+        self.avatarView.centerOnY()
+
+        self.nameLabel.setSize(withWidth: self.contentView.width * 0.6)
+        self.nameLabel.match(.bottom, to: .bottom, of: self.avatarView)
+        self.nameLabel.match(.left, to: .right, of: self.avatarView, offset: Theme.contentOffset)
+
+        self.handleLabel.setSize(withWidth: self.contentView.width * 0.6)
+        self.handleLabel.match(.bottom, to: .top, of: self.nameLabel, offset: -4)
+        self.handleLabel.match(.left, to: .right, of: self.avatarView, offset: Theme.contentOffset)
+
+        self.animationView.squaredSize = 20
+        self.animationView.pin(.right, padding: Theme.contentOffset)
+        self.animationView.centerOnY()
     }
 }
