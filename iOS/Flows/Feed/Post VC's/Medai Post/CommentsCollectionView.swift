304154d2b53daeff8b29949ba82135f6a5cd83af
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-04-09 09:05:01 -0500

    Adding files.

---
+++ iOS/Flows/Feed/Post VC's/Medai Post/CommentsCollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  CommentsCollectionView.swift
+//  Ours
+//
+//  Created by Benji Dodgson on 4/9/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/Flows/Feed/Post VC's/Medai Post/CommentsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CommentsViewController.swift
+//  Ours
+//
+//  Created by Benji Dodgson on 4/9/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
