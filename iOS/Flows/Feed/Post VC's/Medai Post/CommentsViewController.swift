4e503f53a9d99b178256f545879f7ae3a1adae06
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-04-11 19:24:16 -0500

    Added create comment function.

--- iOS/Flows/Feed/Post VC's/Medai Post/CommentsViewController.swift
+++ iOS/Flows/Feed/Post VC's/Medai Post/CommentsViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import Foundation
+import Combine
 
 class CommentsViewController: CollectionViewController<CommentsCollectionViewManager.SectionType, CommentsCollectionViewManager> {
 
     private lazy var commentsCollectionView = CommentsCollectionView(layout: UICollectionViewFlowLayout())
 
+    private let post: Post
+
+    init(with post: Post) {
+        self.post = post
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func getCollectionView() -> CollectionView {
         return self.commentsCollectionView
+    }
+
+    private func createComment(with body: String, replyId: String? = nil) {
+        CreateComment(postId: self.post.objectId!,
+                      body: body,
+                      attributes: [:],
+                      replyId: replyId)
+            .makeRequest(andUpdate: [], viewsToIgnore: [])
+            .mainSink { result in
+                switch result {
+                case .success(_):
+                    break
+                case .error(_):
+                    break
+                }
+            }.store(in: &self.cancellables)
     }
 }
