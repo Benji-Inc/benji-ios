2b375fb0b435292eff7c6b65c2eaa58f5debf8d3
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-05-05 08:53:20 -0500

    fixing issue where sometimes first responder wouldnt go through when viewing a post in a feed.

--- iOS/Flows/Feed/Post VC's/Medai Post/PostMediaViewController.swift
+++ iOS/Flows/Feed/Post VC's/Medai Post/PostMediaViewController.swift
@@ -56,6 +56,14 @@ class PostMediaViewController: PostViewController, CollectionViewInputHandler {
         self.becomeFirstResponder()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        if !self.isFirstResponder {
+            self.becomeFirstResponder()
+        }
+    }
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
 
