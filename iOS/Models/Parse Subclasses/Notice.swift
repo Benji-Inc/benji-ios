30fc09675938807b9ecf62b77b478ad1abfeaad5
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-05-23 12:57:15 -0500

    adding new model

---
+++ iOS/Models/Parse Subclasses/Notice.swift
@@ -0,0 +1,56 @@
+//
+//  Notice.swift
+//  Ours
+//
+//  Created by Benji Dodgson on 5/23/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+enum NoticeKey: String {
+    case type
+    case attributes
+    case priority
+}
+
+final class Notice: PFObject, PFSubclassing, Subscribeable {
+
+    enum NoticeType: String {
+        case alert
+        case connectionRequest
+    }
+
+    static func parseClassName() -> String {
+        return String(describing: self)
+    }
+
+    var type: NoticeType? {
+        get { return self.getObject(for: .type) }
+    }
+
+    var attributes: [String: Any]? {
+        get { self.getObject(for: .attributes) }
+    }
+
+    var priority: Int? {
+        get { self.getObject(for: .priority) }
+    }
+}
+
+extension Notice: Objectable {
+    typealias KeyType = NoticeKey
+
+    func getObject<Type>(for key: NoticeKey) -> Type? {
+        return self.object(forKey: key.rawValue) as? Type
+    }
+
+    func setObject<Type>(for key: NoticeKey, with newValue: Type) {
+        self.setObject(newValue, forKey: key.rawValue)
+    }
+
+    func getRelationalObject<PFRelation>(for key: NoticeKey) -> PFRelation? {
+        return self.relation(forKey: key.rawValue) as? PFRelation
+    }
+}
