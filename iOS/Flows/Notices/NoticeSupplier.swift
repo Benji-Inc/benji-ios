ee175c42dbe414dfd105b24e7a823605e956b6cf
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-05-31 08:27:04 -0700

    Adding supplier

--- iOS/Flows/Notices/NoticeSupplier.swift
+++ iOS/Flows/Notices/NoticeSupplier.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import Combine
+import ParseLiveQuery
+import Parse
 
 class NoticeSupplier {
 
@@ -26,6 +28,49 @@ class NoticeSupplier {
     }
 
     private func subscribeToUpdates() {
+        guard let query = Notice.query() else { return }
+
+        let subscription = Client.shared.subscribe(query)
+
+        subscription.handleEvent { query, event in
+            switch event {
+            case .entered(_):
+                break
+            case .left(let obj), .deleted(let obj):
+                guard let notice = obj as? Notice else { return }
+                break
+//                self.comments.remove(object: comment.systemComment)
+//                runMain {
+//                    self.loadSnapshot()
+//                }
+            case .created(let obj):
+                guard let notice = obj as? Notice else { return }
+                break
+//                var index: Int?
+//                for (indx, existing) in self.comments.enumerated() {
+//                    if existing.updateId == comment.updateId {
+//                        index = indx
+//                    }
+//                }
+//
+//                if let indx = index {
+//                    self.comments[indx] = comment.systemComment
+//                } else {
+//                    self.comments.append(comment.systemComment)
+//                    self.comments.sort()
+//                }
+
+
+            case .updated(let obj):
+//                if let comment = obj as? Comment, let index = self.comments.firstIndex(of: comment.systemComment) {
+//                    self.comments[index] = comment.systemComment
+//                    runMain {
+//                        self.loadSnapshot()
+//                    }
+//                }
+                break
+            }
+        }
 
     }
 }
