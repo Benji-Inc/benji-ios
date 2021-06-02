f77935912d055608ab9adadf5806f0fa6d154cbc
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-06-02 08:52:29 -0700

    adding stationarypress

--- iOS/Flows/Notices/Cells/NoticeCell.swift
+++ iOS/Flows/Notices/Cells/NoticeCell.swift
@@ -8,15 +8,54 @@
 
 import Foundation
 
-class NoticeCell: CollectionViewManagerCell, ManageableCell {
+class NoticeCell: CollectionViewManagerCell, ManageableCell, UIGestureRecognizerDelegate {
     typealias ItemType = SystemNotice
+
+    // Touch Handlers
+    private lazy var stationaryPressRecognizer
+         = StationaryPressGestureRecognizer(cancelsTouchesInView: false,
+                                            target: self,
+                                            action: #selector(self.handleStationaryPress))
 
     var currentItem: SystemNotice?
 
     override func initializeSubviews() {
         super.initializeSubviews()
 
+        self.contentView.addGestureRecognizer(self.stationaryPressRecognizer)
+        self.stationaryPressRecognizer.delegate = self
+
+        self.contentView.layer.borderWidth = 2
+        self.contentView.layer.borderColor = Color.background2.color.cgColor
+        self.contentView.layer.cornerRadius = Theme.cornerRadius
     }
 
     func configure(with item: SystemNotice) {}
+
+    // MARK: Touch Handling
+
+    @objc private func handleStationaryPress(_ gestureRecognizer: StationaryPressGestureRecognizer) {
+        // Scale down the cell when pressed, and scale back up on release.
+        switch gestureRecognizer.state {
+        case .possible, .changed:
+            break
+        case .began:
+            self.scaleDown()
+        case .ended, .cancelled, .failed:
+            self.scaleUp()
+        @unknown default:
+            break
+        }
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+                           shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+
+        if gestureRecognizer === self.stationaryPressRecognizer {
+            if otherGestureRecognizer.view?.isDescendant(of: self) == true {
+                return false
+            }
+        }
+        return true
+    }
 }
