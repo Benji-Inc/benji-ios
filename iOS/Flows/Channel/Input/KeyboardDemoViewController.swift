9458751180454af62ae8b97cbfb7315f2b0167a9
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-06-05 14:00:25 -0700

    adding demo view.

--- iOS/Flows/Channel/Input/KeyboardDemoViewController.swift
+++ iOS/Flows/Channel/Input/KeyboardDemoViewController.swift
@@ -8,11 +8,14 @@
 
 import Foundation
 import TMROLocalization
+import Lottie
 
 class KeyboardDemoViewController: ViewController {
 
     let scrollView = UIScrollView()
     let pagingIndicator = UIPageControl()
+
+    private var demoViews: [DemoView] = []
 
     enum DemoType {
 
@@ -23,11 +26,11 @@ class KeyboardDemoViewController: ViewController {
         var instruction: Localized {
             switch self {
             case .send:
-                return ""
+                return "Send"
             case .sendAlert:
-                return ""
+                return "Alert"
             case .cursor:
-                return ""
+                return "Cursor"
             }
         }
 
@@ -48,10 +51,69 @@ class KeyboardDemoViewController: ViewController {
     }
 
     func load(demos: [DemoType]) {
+        self.demoViews.removeAll()
 
+        self.scrollView.subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+
+        demos.forEach { demo in
+            let view = DemoView(with: demo)
+            self.scrollView.addSubview(view)
+        }
+
+        self.view.layoutNow()
     }
 
     func play() {
+//        delay(2.0) { [unowned self] in
+//            self.play()
+//        }
+    }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        for (index, view) in self.demoViews.enumerated() {
+            let xOffset: CGFloat = self.scrollView.width * CGFloat(index)
+            view.frame = CGRect(x: xOffset,
+                                y: 0, width: self.view.width,
+                                height: self.view.height)
+        }
+
+        self.scrollView.contentSize = CGSize(width: self.view.width * CGFloat((self.demoViews.count - 1)), height: self.view.height)
+    }
+}
+
+class DemoView: View {
+
+    let animationView = AnimationView()
+    let label = Label(font: .regular)
+    private let demo: KeyboardDemoViewController.DemoType
+
+    init(with demo: KeyboardDemoViewController.DemoType) {
+        self.demo = demo
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func initializeSubviews() {
+        super.initializeSubviews()
+
+        self.animationView.load(animation: self.demo.animation)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        self.animationView.centerOnX()
+        self.animationView.centerY = self.height * 0.4
+
+        self.label.setSize(withWidth: self.width - Theme.contentOffset)
+        self.label.centerOnX()
+        self.label.match(.top, to: .bottom, of: self.animationView, offset: Theme.contentOffset.half)
     }
 }
