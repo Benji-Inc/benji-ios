390fe060c90e7eb141acfdbc35d10b7ac1aed772
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-06-18 09:14:43 -0700

    adding the intent handler placeholders.

---
+++ iOS/Intent Handlers/MessageIntentHandler.swift
@@ -0,0 +1,14 @@
+//
+//  MessageIntentHandler.swift
+//  Ours
+//
+//  Created by Benji Dodgson on 6/18/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+
+class MessageIntentHandler {
+    
+}
