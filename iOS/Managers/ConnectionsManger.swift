38664f18a5d081520cceb8113d5fa06c1cd3c782
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-06-18 15:14:08 -0700

    adding a connections manager

---
+++ iOS/Managers/ConnectionsManger.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Ours
+//
+//  Created by Benji Dodgson on 6/18/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+class ConnectionsManager {
+
+    static let shared = ConnectionsManager()
+
+    init() {
+        self.subscribeToUpdates()
+    }
+
+    private func subscribeToUpdates() {
+
+    }
+}
