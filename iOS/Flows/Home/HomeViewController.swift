f18014c70cac97733ac427fe873d9df75b6a3b14
Martin Young <88675954+MartinJibber@users.noreply.github.com>
2021-09-01 16:35:10 -0700

    Refactoring the home view controller to use a new CollectionViewDataSource class (#29)
    * Refactoring the home view controller to use the new collection view data source pattern.
    
    * Created a collection view data source base class to replace the data source creator protocol and updated the home view controller to use this instead.
    
    * Minor documentation change.
    
    * Fixing a bug in the selectable extension that could causes a tap handler to be called more than once. Removing the home collectionview manager. Restoring contextual menu functionality to the home view controller.
    
    * Making home collection view layout init non static. Removing home collection view.
    
    * Removing collection view property from the collection view data source.
    
    * Minor renaming of generic types in collectionview data source for consistency.
    
    * Refactoring home collection data source to reduce the need for casting the items.
    
    * Fixing typo.

--- iOS/Flows/Home/HomeViewController.swift
+++ iOS/Flows/Home/HomeViewController.swift
@@ -7,54 +7,59 @@
 //
 
 import Foundation
-import Contacts
 import Parse
-import Combine
-import Intents
 
-class HomeViewController: CollectionViewController<HomeCollectionViewManager.SectionType,
-                          HomeCollectionViewManager>, TransitionableViewController {
+protocol HomeViewControllerDelegate: AnyObject {
+    func homeViewControllerDidTapAdd(_ controller: HomeViewController)
+    func homeViewControllerDidSelectReservations(_ controller: HomeViewController)
+    func homeViewControllerDidSelect(item: HomeCollectionViewDataSource.ItemType)
+}
 
-    var receivingPresentationType: TransitionType {
-        return .home
-    }
+class HomeViewController: ViewController {
 
-    var transitionColor: Color {
-        return .background1
-    }
+    weak var delegate: HomeViewControllerDelegate?
+
+    // MARK: - UI
+
+    private lazy var dataSource = HomeCollectionViewDataSource(collectionView: self.collectionView)
+    private var collectionView = CollectionView(layout: HomeCollectionViewLayout())
 
     let addButton = Button()
 
-    var isMenuPresenting: Bool = false
+    // MARK: - Lifecycle
 
     override func initializeViews() {
         super.initializeViews()
 
-        ConnectionsManager.shared.$userUpdated.mainSink { user in
-            guard let u = user else { return }
-            print("FOCUS: \(String(describing: u.focusStatus))")
-        }.store(in: &self.cancellables)
-
         self.view.set(backgroundColor: .background1)
 
-        self.view.insertSubview(self.addButton, aboveSubview: self.collectionViewManager.collectionView)
-        self.addButton.set(style: .icon(image: UIImage(systemName: "plus")!, color: .lightPurple))
-    }
+        self.view.addSubview(self.collectionView)
 
-    override func getCollectionView() -> CollectionView {
-        return HomeCollectionView()
+        self.view.addSubview(self.addButton)
+        self.addButton.set(style: .icon(image: UIImage(systemName: "plus")!, color: .lightPurple))
+        self.addButton.addAction(for: .touchUpInside) { [unowned self] in
+            self.delegate?.homeViewControllerDidTapAdd(self)
+        }
+
+        self.dataSource.didSelectReservations = { [unowned self] in
+            self.delegate?.homeViewControllerDidTapAdd(self)
+        }
+
+        self.collectionView.delegate = self
     }
 
     override func viewWasPresented() {
         super.viewWasPresented()
 
         Task {
-            await self.collectionViewManager.load()
+            await self.loadData()
         }
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+
+        self.collectionView.expandToSuperviewSize()
 
         self.addButton.squaredSize = 60
         self.addButton.makeRound()
@@ -62,10 +67,168 @@ class HomeViewController: CollectionViewController<HomeCollectionViewManager.Sec
         self.addButton.pinToSafeArea(.bottom, padding: Theme.contentOffset)
     }
 
-    func animate(show: Bool) {
-        self.isMenuPresenting = !show
-        UIView.animate(withDuration: Theme.animationDuration) {
-            self.collectionViewManager.collectionView.alpha = show ? 1.0 : 0.0
+    // MARK: Data Loading
+
+    @MainActor
+    private func loadData() async {
+        self.collectionView.animationView.play()
+
+        async let unclaimedReservationCount = Reservation.getUnclaimedReservationCount(for: User.current()!)
+        async let initialSyncFinished: Void = ChannelSupplier.shared.waitForInitialSync()
+        async let noticesLoaded: Void = NoticeSupplier.shared.loadNotices()
+
+        let _ = await (initialSyncFinished, noticesLoaded)
+
+        self.dataSource.unclaimedCount = await unclaimedReservationCount
+        
+        let cycle = AnimationCycle(inFromPosition: .inward,
+                                   outToPosition: .inward,
+                                   shouldConcatenate: true,
+                                   scrollToEnd: false)
+
+        let snapshot = self.getInitialSnapshot()
+        await self.dataSource.apply(snapshot, collectionView: self.collectionView, animationCycle: cycle)
+
+        self.collectionView.animationView.stop()
+    }
+
+    private func getInitialSnapshot() -> NSDiffableDataSourceSnapshot<HomeCollectionViewDataSource.SectionType,
+                                                                      HomeCollectionViewDataSource.ItemType> {
+        var snapshot = self.dataSource.snapshot()
+
+        let allCases = HomeCollectionViewDataSource.SectionType.allCases
+        snapshot.appendSections(allCases)
+        allCases.forEach { (section) in
+            snapshot.appendItems(self.getItems(for: section), toSection: section)
         }
+
+        return snapshot
+    }
+
+    private func getItems(for section: HomeCollectionViewDataSource.SectionType)
+    -> [HomeCollectionViewDataSource.ItemType] {
+
+        switch section {
+        case .notices:
+            return NoticeSupplier.shared.notices.map { notice in
+                return .notice(notice)
+            }
+        case .channels:
+            return ChannelSupplier.shared.allChannelsSorted.map { channel in
+                return .channel(channel)
+            }
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let identifier = self.dataSource.itemIdentifier(for: indexPath) else { return }
+
+        self.delegate?.homeViewControllerDidSelect(item: identifier)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        contextMenuConfigurationForItemAt indexPath: IndexPath,
+                        point: CGPoint) -> UIContextMenuConfiguration? {
+
+        guard let channel = ChannelSupplier.shared.allChannelsSorted[safe: indexPath.row],
+              let cell = collectionView.cellForItem(at: indexPath) as? ChannelCell else { return nil }
+
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: {
+            return ChannelPreviewViewController(with: channel, size: cell.size)
+        }, actionProvider: { suggestedActions in
+            if channel.isFromCurrentUser {
+                return self.makeCurrentUserMenu(for: channel, at: indexPath)
+            } else {
+                return self.makeNonCurrentUserMenu(for: channel, at: indexPath)
+            }
+        })
+    }
+
+    func makeCurrentUserMenu(for channel: DisplayableChannel, at indexPath: IndexPath) -> UIMenu {
+        let neverMind = UIAction(title: "Never Mind", image: UIImage(systemName: "nosign")) { _ in }
+
+        let confirm = UIAction(title: "Confirm",
+                               image: UIImage(systemName: "trash"),
+                               attributes: .destructive) { action in
+
+            switch channel.channelType {
+            case .system(_):
+                break
+            case .pending(_):
+                break
+            case .channel(let tchChannel):
+                Task {
+                    do {
+                        try await ChannelSupplier.shared.delete(channel: tchChannel)
+                    } catch {
+                        logDebug(error)
+                    }
+                }
+            }
+        }
+
+        let deleteMenu = UIMenu(title: "Delete",
+                                image: UIImage(systemName: "trash"),
+                                options: .destructive,
+                                children: [confirm, neverMind])
+
+        let open = UIAction(title: "Open", image: UIImage(systemName: "arrowshape.turn.up.right")) { [unowned self] _ in
+            guard let identifier = self.dataSource.itemIdentifier(for: indexPath) else { return }
+            self.delegate?.homeViewControllerDidSelect(item: identifier)
+        }
+
+        // Create and return a UIMenu with the share action
+        return UIMenu(title: "Options", children: [open, deleteMenu])
+    }
+
+    func makeNonCurrentUserMenu(for channel: DisplayableChannel, at indexPath: IndexPath) -> UIMenu {
+
+        let neverMind = UIAction(title: "Never Mind", image: UIImage(systemName: "nosign")) { _ in }
+
+        let confirm = UIAction(title: "Confirm",
+                               image: UIImage(systemName: "clear"),
+                               attributes: .destructive) { action in
+
+            switch channel.channelType {
+            case .system(_):
+                break
+            case .pending(_):
+                break
+            case .channel(let tchChannel):
+                Task {
+                    do {
+                        try await ChannelSupplier.shared.delete(channel: tchChannel)
+                    } catch {
+                        logDebug(error)
+                    }
+                }
+            }
+        }
+
+        let deleteMenu = UIMenu(title: "Leave",
+                                image: UIImage(systemName: "clear"),
+                                options: .destructive,
+                                children: [confirm, neverMind])
+
+        let open = UIAction(title: "Open", image: UIImage(systemName: "arrowshape.turn.up.right")) { [unowned self] _ in
+            guard let item = self.dataSource.itemIdentifier(for: indexPath) else { return }
+            self.delegate?.homeViewControllerDidSelect(item: item)
+        }
+
+        // Create and return a UIMenu with the share action
+        return UIMenu(title: "Options", children: [open, deleteMenu])
+    }
+}
+
+extension HomeViewController: TransitionableViewController {
+    var receivingPresentationType: TransitionType {
+        return .home
+    }
+
+    var transitionColor: Color {
+        return .background1
     }
 }
