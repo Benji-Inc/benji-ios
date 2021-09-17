54ac7efc13a639eeed200e8e9e0f00c8007a304b
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-09-17 09:17:33 -0700

    displaying conversation from archive

--- iOS/Flows/Archives/ArchiveCoordinator.swift
+++ iOS/Flows/Archives/ArchiveCoordinator.swift
@@ -30,5 +30,28 @@ extension ArchiveCoordinator: ArchiveViewControllerDelegate {
 
     nonisolated func archiveView(_ controller: ArchiveViewController, didSelect item: ArchiveCollectionViewDataSource.ItemType) {
 
+        switch item {
+        case .conversation(let conversation):
+            Task.onMainActor {
+                self.startConversationFlow(for: conversation.conversationType)
+            }
+        }
+    }
+
+    func startConversationFlow(for type: ConversationType?) {
+        self.removeChild()
+        var conversation: DisplayableConversation?
+        if let t = type {
+            conversation = DisplayableConversation(conversationType: t)
+        }
+        let coordinator = ConversationCoordinator(router: self.router,
+                                                  deepLink: self.deepLink,
+                                                  conversation: conversation)
+        self.addChildAndStart(coordinator, finishedHandler: { (_) in
+            self.router.dismiss(source: coordinator.toPresentable(), animated: true) {
+                self.finishFlow(with: ())
+            }
+        })
+        self.router.present(coordinator, source: self.archiveVC, animated: true)
     }
 }
