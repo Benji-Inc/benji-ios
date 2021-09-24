71b4ea7353eef476e2c60ded8349bdb118139e4d
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-09-24 11:27:17 -0700

    adding layout class.

---
+++ iOS/Flows/People/PeopleCollectionViewLayout.swift
@@ -0,0 +1,60 @@
+//
+//  PeopleCollectionViewLayout.swift
+//  Jibber
+//
+//  Created by Benji Dodgson on 9/24/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+class PeopleCollectionViewLayout: UICollectionViewCompositionalLayout {
+
+    init() {
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.scrollDirection = .vertical
+
+        super.init(sectionProvider: { sectionIndex, environment in
+            guard let sectionType = PeopleCollectionViewDataSource.SectionType(rawValue: sectionIndex) else { return nil }
+
+            switch sectionType {
+            case .connections:
+                // Item
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: Theme.contentOffset, bottom: 8, trailing: Theme.contentOffset)
+
+                // Group
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+                // Section
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(top: Theme.contentOffset - 8, leading: 0, bottom: Theme.contentOffset.doubled + Theme.contentOffset, trailing: 0)
+
+                return section
+
+            case .contacts:
+                // Item
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: Theme.contentOffset, bottom: 8, trailing: Theme.contentOffset)
+
+                // Group
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+                // Section
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(top: Theme.contentOffset - 8, leading: 0, bottom: Theme.contentOffset.doubled + Theme.contentOffset, trailing: 0)
+
+                return section
+            }
+
+        }, configuration: config)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
