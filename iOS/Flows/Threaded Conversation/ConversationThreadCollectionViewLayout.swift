b0d1b51573c9df1fc01f1b97b2a8f622755e1691
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-10-10 11:06:27 -0700

    adding new collection view layout

--- iOS/Flows/Threaded Conversation/ConversationThreadCollectionViewLayout.swift
+++ iOS/Flows/Threaded Conversation/ConversationThreadCollectionViewLayout.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class ConversationThreadCollectionViewLayout: UICollectionViewCompositionalLayout {
+
+    init() {
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.scrollDirection = .vertical
+
+        super.init(sectionProvider: { sectionIndex, environment in
+
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: Theme.contentOffset, bottom: 8, trailing: Theme.contentOffset)
+
+            // Group
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(1))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+            // Section
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 200, leading: 0, bottom: 200, trailing: 0)
+
+            return section
+
+        }, configuration: config)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
