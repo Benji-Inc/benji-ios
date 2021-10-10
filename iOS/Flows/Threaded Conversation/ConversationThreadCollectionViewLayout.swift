f9531039666332750184d38e9556d445f9b3439d
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-10-10 10:59:20 -0700

    removing the extra data source.

---
+++ iOS/Flows/Threaded Conversation/ConversationThreadCollectionViewLayout.swift
@@ -0,0 +1,9 @@
+//
+//  ConversationThreadCollectionViewLayout.swift
+//  Jibber
+//
+//  Created by Benji Dodgson on 10/10/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
