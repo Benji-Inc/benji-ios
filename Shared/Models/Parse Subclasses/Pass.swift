9572fce9cac91e5513c3c1d559f0cb4150e8057c
Benjamin Dodgson <benjamindodgson@gmail.com>
2021-10-15 16:39:31 -0700

    Adding pass class

---
+++ Shared/Models/Parse Subclasses/Pass.swift
@@ -0,0 +1,47 @@
+//
+//  Pass.swift
+//  Jibber
+//
+//  Created by Benji Dodgson on 10/15/21.
+//  Copyright © 2021 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+enum PassKey: String {
+    case owner
+    case attributes
+}
+
+final class Pass: PFObject, PFSubclassing {
+
+    static func parseClassName() -> String {
+        return String(describing: self)
+    }
+
+    var owner: User? {
+        return self.getObject(for: .owner)
+    }
+
+    var attributes: [String: Any]? {
+        return self.getObject(for: .attributes)
+    }
+}
+
+extension Pass: Objectable {
+    typealias KeyType = PassKey
+
+    func getObject<Type>(for key: PassKey) -> Type? {
+        return self.object(forKey: key.rawValue) as? Type
+    }
+
+    func setObject<Type>(for key: PassKey, with newValue: Type) {
+        self.setObject(newValue, forKey: key.rawValue)
+    }
+
+    func getRelationalObject<PFRelation>(for key: PassKey) -> PFRelation? {
+        return self.relation(forKey: key.rawValue) as? PFRelation
+    }
+}
+
