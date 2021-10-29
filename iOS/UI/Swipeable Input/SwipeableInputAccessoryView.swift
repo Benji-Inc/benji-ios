a5570491e8593166ce385fc8e22589124e48d2a2
Martin Young <martin@jibber.wtf>
2021-10-29 10:34:37 -0700

    Work toward improving resizing behavior of the swipe input view.

--- iOS/UI/Swipeable Input/SwipeableInputAccessoryView.swift
+++ iOS/UI/Swipeable Input/SwipeableInputAccessoryView.swift
@@ -35,13 +35,16 @@ class SwipeableInputAccessoryView: View, AttachmentViewControllerDelegate, UIGes
 
     }
 
+    /// The location on the screen that a send action was triggered.
     enum SendPosition {
         case left
         case middle
         case right
     }
 
+    /// The maximum height we should allow this view to expand to.
     static let maxHeight: CGFloat = 500.0
+    static let bottomPadding: CGFloat = 8
 
     var alertAnimator: UIViewPropertyAnimator?
     var selectionFeedback = UIImpactFeedbackGenerator(style: .rigid)
@@ -64,23 +67,6 @@ class SwipeableInputAccessoryView: View, AttachmentViewControllerDelegate, UIGes
     var currentMessageKind: MessageKind = .text(String())
     private var sendable: SendableObject?
 
-    override var intrinsicContentSize: CGSize {
-        // Calculate intrinsicContentSize that will fit all the text
-        var contentHeight = self.textView.sizeThatFits(CGSize(width: self.width - Theme.contentOffset.doubled,
-                                                              height: .greatestFiniteMagnitude)).height
-        if contentHeight == 0 {
-            contentHeight = 33
-        }
-
-        contentHeight += InputActivityBar.height
-        + self.inputContainerView.tailLength
-        + self.safeAreaInsets.bottom
-
-        logDebug("intrinsic content height is \(contentHeight)")
-
-        return CGSize(width: UIView.noIntrinsicMetric, height: contentHeight)
-    }
-
     unowned let delegate: SwipeableInputAccessoryViewDelegate
 
     init(with delegate: SwipeableInputAccessoryViewDelegate) {
@@ -92,11 +78,12 @@ class SwipeableInputAccessoryView: View, AttachmentViewControllerDelegate, UIGes
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: View Setup and Layout
+
     override func initializeSubviews() {
         super.initializeSubviews()
 
         self.backgroundColor = .red
-        self.autoresizingMask = .flexibleHeight
 
         self.addSubview(self.activityBar)
         self.addSubview(self.inputContainerView)
@@ -116,6 +103,32 @@ class SwipeableInputAccessoryView: View, AttachmentViewControllerDelegate, UIGes
         self.setupHandlers()
     }
 
+    private var heightConstraint: NSLayoutConstraint?
+
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+
+        // Get a reference to the height constraint so we can expand this view for text input
+        self.heightConstraint = self.constraints.first(where: { constraint in
+            return constraint.firstAttribute == .height
+        })
+        self.heightConstraint?.constant = self.getContentHeight()
+    }
+
+    private func getContentHeight() -> CGFloat {
+        // Calculate intrinsicContentSize that will fit all the text
+        var textViewHeight = self.textView.sizeThatFits(CGSize(width: self.width - Theme.contentOffset.doubled,
+                                                               height: .greatestFiniteMagnitude)).height
+        if textViewHeight == 0 {
+            textViewHeight = 33
+        }
+
+        let contentHeight = textViewHeight + InputActivityBar.height + self.inputContainerView.tailLength
+        + SwipeableInputAccessoryView.bottomPadding
+
+        return contentHeight
+    }
+
     override func layoutSubviews() {
         super.layoutSubviews()
 
@@ -131,7 +144,7 @@ class SwipeableInputAccessoryView: View, AttachmentViewControllerDelegate, UIGes
         self.inputContainerView.pin(.left, padding: horizontalOffset)
         self.inputContainerView.match(.top, to: .bottom, of: self.activityBar)
         self.inputContainerView.expand(.right, padding: horizontalOffset)
-        self.inputContainerView.expand(.bottom, padding: self.safeAreaInsets.bottom)
+        self.inputContainerView.expand(.bottom, padding: SwipeableInputAccessoryView.bottomPadding)
 
         // TODO: Find a more elegant solution than manually calling layout now.
         self.inputContainerView.layoutNow()
@@ -139,6 +152,7 @@ class SwipeableInputAccessoryView: View, AttachmentViewControllerDelegate, UIGes
         self.textView.expandToSuperviewSize()
 
         self.blurView.expandToSuperviewSize()
+        self.blurView.roundCorners()
 
         self.overlayButton.expandToSuperviewSize()
 
@@ -220,7 +234,8 @@ class SwipeableInputAccessoryView: View, AttachmentViewControllerDelegate, UIGes
             break
         }
 
-        self.invalidateIntrinsicContentSize()
+        // Update the height of the view to account for the new text.
+        self.heightConstraint?.constant = self.getContentHeight()
     }
 
     func getDataTypes(from text: String) -> [NSTextCheckingResult]? {
