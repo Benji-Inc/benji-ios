ea22dd4b78cd51bf84621131bef5d6c9c099242b
Benji <benji@jibber.wtf>
2021-12-13 08:36:50 -0800

    adding emotions.

--- iOS/Models/ReactionType.swift
+++ iOS/Models/ReactionType.swift
@@ -56,3 +56,86 @@ enum ReactionType: String, CaseIterable {
         }
     }
 }
+
+enum Emotion: String {
+    case appreciation
+    case amusement
+    case anger
+    case anxiety
+    case awe
+    case awkwardness
+    case boredom
+    case calmness
+    case confusion
+    case craving
+    case disgust
+    case empathy
+    case entrancement
+    case excitement
+    case fear
+    case horror
+    case interest
+    case joy
+    case nostalgia
+    case relief
+    case romance
+    case sadness
+    case satisfaction
+    case desire
+    case suprise
+    
+    var emoji: String {
+        switch self {
+        case .appreciation:
+            return "☺️"
+        case .amusement:
+            return "😂"
+        case .anger:
+            return "😡"
+        case .anxiety:
+            return "😓"
+        case .awe:
+            return "😳"
+        case .awkwardness:
+            return "🥴"
+        case .boredom:
+            return "🥱"
+        case .calmness:
+            return "😌"
+        case .confusion:
+            return "🤔"
+        case .craving:
+            return "😋"
+        case .disgust:
+            return "😖"
+        case .empathy:
+            return "😔"
+        case .entrancement:
+            return "🤪"
+        case .excitement:
+            return "🤩"
+        case .fear:
+            return "😰"
+        case .horror:
+            return "😱"
+        case .interest:
+            return "🧐"
+        case .joy:
+            return "🥳"
+        case .nostalgia:
+            return "🤠"
+        case .relief:
+            return "😅"
+        case .romance:
+            return "😍"
+        case .sadness:
+            return "😥"
+        case .satisfaction:
+            return "🥰"
+        case .desire:
+            return "😈"
+        case .suprise:
+            return "😮"
+        }
+    }
+}
