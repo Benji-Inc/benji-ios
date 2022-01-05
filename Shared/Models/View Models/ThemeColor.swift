d7fa0f92adcd6d38d3be35a0c34032ae0f6ae328
Benji <benji@jibber.wtf>
2022-01-05 10:46:11 -0800

    adding background gradient.

--- Shared/Models/View Models/ThemeColor.swift
+++ Shared/Models/View Models/ThemeColor.swift
@@ -10,6 +10,20 @@ import Foundation
 import UIKit
 
 enum ThemeColor: String, CaseIterable {
+    
+    case B1
+    case B2
+    case D1
+    case D2
+    case D3
+    case D4
+    case D5
+    case D6
+    case L1
+    case L2
+    case L3
+    case L4
+    case L5
 
     case border
     case background
@@ -40,6 +54,33 @@ enum ThemeColor: String, CaseIterable {
         case .clear:
             return UIColor(named: "CLEAR")!
         case .red:
+            return UIColor(named: "RED")!
+            
+        case .B1:
+            return UIColor(named: "RED")!
+        case .B2:
+            return UIColor(named: "RED")!
+        case .D1:
+            return UIColor(named: "RED")!
+        case .D2:
+            return UIColor(named: "RED")!
+        case .D3:
+            return UIColor(named: "RED")!
+        case .D4:
+            return UIColor(named: "RED")!
+        case .D5:
+            return UIColor(named: "RED")!
+        case .D6:
+            return UIColor(named: "RED")!
+        case .L1:
+            return UIColor(named: "RED")!
+        case .L2:
+            return UIColor(named: "RED")!
+        case .L3:
+            return UIColor(named: "RED")!
+        case .L4:
+            return UIColor(named: "RED")!
+        case .L5:
             return UIColor(named: "RED")!
         }
     }
