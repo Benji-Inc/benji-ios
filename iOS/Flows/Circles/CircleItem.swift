e55a51fe9d44fcdfa3f5dd3ce00d0b7a112bb2e1
Benji <benji@jibber.wtf>
2022-01-27 10:46:26 -0800

    adding avatar conformance.

--- iOS/Flows/Circles/CircleItem.swift
+++ iOS/Flows/Circles/CircleItem.swift
@@ -9,7 +9,40 @@
 import Foundation
 import Contacts
 
-struct CircleItem: Hashable {
+struct CircleItem: Avatar, Hashable {
+    
+    var givenName: String {
+        if let user = self.user {
+            return user.givenName
+        } else if let contact = self.contact {
+            return contact.givenName
+        } else {
+            return ""
+        }
+    }
+    
+    var familyName: String {
+        if let user = self.user {
+            return user.familyName
+        } else if let contact = self.contact {
+            return contact.familyName
+        } else {
+            return ""
+        }
+    }
+    
+    var handle: String {
+        return self.user?.handle ?? ""
+    }
+    
+    var userObjectId: String? {
+        return self.user?.objectId ?? nil
+    }
+    
+    var image: UIImage? {
+        return self.user?.image
+    }
+    
     var position: Int 
     var user: User?
     var contact: CNContact? 
