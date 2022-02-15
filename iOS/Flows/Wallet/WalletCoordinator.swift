6f512ac632b67bc2468b66a02b2acf462b445d4d
Benji <benji@jibber.wtf>
2022-02-14 17:22:52 -0800

    adding detail disclosure.

--- iOS/Flows/Wallet/WalletCoordinator.swift
+++ iOS/Flows/Wallet/WalletCoordinator.swift
@@ -15,4 +15,26 @@ class WalletCoordinator: PresentableCoordinator<Void> {
     override func toPresentable() -> DismissableVC {
         return self.walletVC
     }
+    
+    override func start() {
+        super.start()
+        
+        self.walletVC.dataSource.didTapDetail = {
+            self.presentAlert()
+        }
+    }
+    
+    private func presentAlert() {
+                
+        let alertController = UIAlertController(title: "Jibs",
+                                                message: "Earn Jibs now and soon you can use them to upgrade, vote on features, or invest in Jibber.",
+                                                preferredStyle: .alert)
+
+        let cancelAction = UIAlertAction(title: "Got it", style: .cancel, handler: {
+            (action : UIAlertAction!) -> Void in
+        })
+
+        alertController.addAction(cancelAction)
+        self.walletVC.present(alertController, animated: true, completion: nil)
+    }
 }
--- iOS/Flows/Wallet/WalletHeaderView.swift
+++ iOS/Flows/Wallet/WalletHeaderView.swift
@@ -12,10 +12,26 @@ class WalletHeaderView: UICollectionReusableView {
     
     private let topLeftDetailView = DetailView(shouldPinLeft: true)
     private let bottomLeftDetailView = DetailView(shouldPinLeft: true)
-    private let topRightDetailView = DetailView(shouldPinLeft: false)
+    private let topRightDetailView = DetailView(shouldPinLeft: false, showDetail: true)
     private let bottomRightDetailView = DetailView(shouldPinLeft: false)
-    
     private let imageView = UIImageView(image: UIImage(named: "jiblogo"))
+    
+    var didTapDetail: CompletionOptional = nil
+    
+    private let totalFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 6
+        return formatter
+    }()
+    
+    private let balanceFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "usd"
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,6 +49,10 @@ class WalletHeaderView: UICollectionReusableView {
         self.addSubview(self.topRightDetailView)
         self.addSubview(self.bottomRightDetailView)
         self.addSubview(self.imageView)
+        
+        self.topRightDetailView.didSelect { [unowned self] in
+            self.didTapDetail?()
+        }
     }
     
     func configure(with transactions: [WalletCollectionViewDataSource.ItemType]) {
@@ -42,19 +62,25 @@ class WalletHeaderView: UICollectionReusableView {
             let createdAt = user.createdAt else { return }
             
             let dateString = Date.monthYear.string(from: createdAt)
-            self.bottomLeftDetailView.configure(with: "Jibber", subtitle: "Member since \(dateString)")
-
-            if let total = try? await self.calculateEarnings(for: transactions) {
-                self.topRightDetailView.configure(with: "\(total)", subtitle: "Total Earnings")
+            
+            if let memberPosition = User.current()?.quePosition {
+                self.bottomLeftDetailView.configure(with: "#\(memberPosition)", subtitle: "Member since \(dateString)")
             } else {
-                self.topRightDetailView.configure(with: "0.0", subtitle: "Total Earnings")
+                self.bottomLeftDetailView.configure(with: "Jibber", subtitle: "Member since \(dateString)")
             }
-            self.bottomRightDetailView.configure(with: "$0.00", subtitle: "Credit Balance")
+
+            let total = try? await self.calculateEarnings(for: transactions)
+            if let stringTotal = self.totalFormatter.string(from: total ?? 0.0) {
+                self.topRightDetailView.configure(with: stringTotal, subtitle: "Earned for activity")
+            }
+
+            let balance = self.balanceFormatter.string(from: self.calculateCreditBalance(for: total ?? 0.0)) ?? "$0.00"
+            self.bottomRightDetailView.configure(with: balance, subtitle: "Credit Balance")
             self.layoutNow()
         }
     }
     
-    func calculateEarnings(for items: [WalletCollectionViewDataSource.ItemType]) async throws -> Double {
+    func calculateEarnings(for items: [WalletCollectionViewDataSource.ItemType]) async throws -> NSNumber {
         let transactions: [Transaction] = try await items.asyncMap { type in
             switch type {
             case .transaction(let transaction):
@@ -67,8 +93,12 @@ class WalletHeaderView: UICollectionReusableView {
             total += transaction.amount
         }
         
-        return total
-        
+        return NSNumber(value: total)
+    }
+    
+    func calculateCreditBalance(for total: NSNumber) -> NSNumber {
+        let multiplier: Double = 10000000
+        return NSNumber(value: total.doubleValue / multiplier)
     }
     
     override func layoutSubviews() {
@@ -96,10 +126,14 @@ class WalletHeaderView: UICollectionReusableView {
 private class DetailView: BaseView {
     let titleLabel = ThemeLabel(font: .medium)
     let subtitleLabel = ThemeLabel(font: .small, textColor: .D1)
+    private let detailDisclosure = UIImageView(image: UIImage(systemName: "info.circle"))
+
     private let shouldPinLeft: Bool
+    private let showDetail: Bool
     
-    init(shouldPinLeft: Bool) {
+    init(shouldPinLeft: Bool, showDetail: Bool = false) {
         self.shouldPinLeft = shouldPinLeft
+        self.showDetail = showDetail
         super.init()
     }
     
@@ -110,6 +144,9 @@ private class DetailView: BaseView {
     override func initializeSubviews() {
         super.initializeSubviews()
         
+        self.addSubview(self.detailDisclosure)
+        self.tintColor = ThemeColor.T1.color
+        self.detailDisclosure.isHidden = !self.showDetail
         self.addSubview(self.titleLabel)
         self.addSubview(self.subtitleLabel)
     }
@@ -121,6 +158,7 @@ private class DetailView: BaseView {
         self.titleLabel.textAlignment = self.shouldPinLeft ? .left : .right
         self.subtitleLabel.setText(subtitle)
         self.subtitleLabel.textAlignment = self.shouldPinLeft ? .left : .right
+        
         self.layoutNow()
     }
     
@@ -131,7 +169,11 @@ private class DetailView: BaseView {
         self.subtitleLabel.setSize(withWidth: 200)
         
         self.height = self.titleLabel.height + Theme.ContentOffset.short.value + self.subtitleLabel.height
-        self.width = self.titleLabel.width > self.subtitleLabel.width ? self.titleLabel.width : self.subtitleLabel.width
+        if self.showDetail {
+            self.width = self.titleLabel.width + Theme.ContentOffset.short.value + 18
+        } else {
+            self.width = self.titleLabel.width > self.subtitleLabel.width ? self.titleLabel.width : self.subtitleLabel.width
+        }
         
         if self.shouldPinLeft {
             self.titleLabel.pin(.left)
@@ -143,5 +185,9 @@ private class DetailView: BaseView {
         
         self.titleLabel.pin(.top)
         self.subtitleLabel.pin(.bottom)
+        
+        self.detailDisclosure.squaredSize = 18
+        self.detailDisclosure.centerY = self.titleLabel.centerY
+        self.detailDisclosure.match(.right, to: .left, of: self.titleLabel, offset: .negative(.short))
     }
 }
