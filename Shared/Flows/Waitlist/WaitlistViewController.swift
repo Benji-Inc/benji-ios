172e90aa1c1505c093337c12d8c56a27b98ba74e
Benji <benji@jibber.wtf>
2022-04-12 09:56:24 -0700

    adding the waitlist coordinator and vc.

---
+++ Shared/Flows/Waitlist/WaitlistViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WaitlistViewController.swift
+//  Jibber
+//
+//  Created by Benji Dodgson on 4/12/22.
+//  Copyright © 2022 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+class WaitlistViewController: ViewController {
+    
+    override func initializeViews() {
+        super.initializeViews()
+        
+        self.view.set(backgroundColor: .red)
+    }
+}
