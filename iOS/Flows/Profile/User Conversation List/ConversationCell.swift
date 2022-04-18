e020ca7fdcd2f6408c39f0c64aa1dcf0e87ee1c3
Benji <benji@jibber.wtf>
2022-04-18 13:41:23 -0700

    updating the members in the conversation cell as well.

--- iOS/Flows/Profile/User Conversation List/ConversationCell.swift
+++ iOS/Flows/Profile/User Conversation List/ConversationCell.swift
@@ -152,6 +152,25 @@ class ConversationCell: CollectionViewManagerCell, ManageableCell {
         }
         
         self.conversationController?
+            .memberEventPublisher
+            .mainSink(receiveValue: { [unowned self] event in
+                guard let conversationController = self.conversationController else {
+                    return
+                }
+                switch event {
+                case _ as MemberAddedEvent, _ as MemberRemovedEvent:
+                    let members = conversationController.conversation.lastActiveMembers.filter { member in
+                        return member.personId != ChatClient.shared.currentUserId
+                    }
+                    
+                    self.stackedAvatarView.configure(with: members)
+                default:
+                    break
+                }
+                
+            }).store(in: &self.subscriptions)
+        
+        self.conversationController?
             .channelChangePublisher
             .mainSink(receiveValue: { [unowned self] _ in
                 guard let conversationController = self.conversationController else { return }
@@ -161,6 +180,9 @@ class ConversationCell: CollectionViewManagerCell, ManageableCell {
                     self.update(for: latest)
                 }
                 self.setNumberOfUnread(value: conversationController.conversation.totalUnread)
+                
+                
+                
             }).store(in: &self.subscriptions)
         
         self.conversationController?
