8bb89e1ffd00cf698d429de6fbe10760ec5db811
Benji <benji@jibber.wtf>
2022-05-18 14:33:53 -0700

    applying init setup

--- iOS/Flows/Media/MediaCoordinator.swift
+++ iOS/Flows/Media/MediaCoordinator.swift
@@ -17,60 +17,9 @@ class MediaCoordinator: PresentableCoordinator<Void> {
 
     let message: Messageable
     
-    let animationView = AnimationView.with(animation: .loading)
-
-    lazy var imageViewController: MediaViewController = {
-        let images: [LightboxImage]
-        images = self.items.compactMap({ item in
-            guard let url = item.url else { return nil }
-            switch item.type {
-            case .photo:
-                return LightboxImage(imageURL: url, text: self.message.kind.text)
-            case .video:
-                guard let imageURL = item.previewURL else { return nil }
-                return LightboxImage(imageURL: imageURL, text: self.message.kind.text, videoURL: item.url)
-            }
-        })
-
-        var startIndex: Int = 0
-        if let start = self.startingItem {
-            for (index, item) in self.items.enumerated() {
-                if item.url == start.url {
-                    startIndex = index
-                    break
-                }
-            }
-        }
-        
-        LightboxConfig.PageIndicator.textAttributes = [.font: FontType.xtraSmall.font,
-                                                       .foregroundColor: ThemeColor.white.color.withAlphaComponent(0.2)]
-        
-        LightboxConfig.InfoLabel.textAttributes = [.font: FontType.small.font,
-                                                   .foregroundColor: ThemeColor.white.color]
-        
-        LightboxConfig.CloseButton.text = ""
-        LightboxConfig.CloseButton.image = UIImage(systemName: "xmark")
-        LightboxConfig.CloseButton.size = CGSize(width: 20, height: 18)
-        
-        LightboxConfig.makeLoadingIndicator = { [unowned self] in
-            self.animationView
-        }
-        
-        self.animationView.loopMode = .loop
-        self.animationView.play()
-        self.animationView.squaredSize = 18
-
-        // Create an instance of LightboxController.
-        let controller = MediaViewController(images: images, startIndex: startIndex)
-        controller.headerView.closeButton.tintColor = ThemeColor.white.color
-        controller.headerView.closeButton.imageView?.contentMode = .scaleAspectFit
-
-        // Use dynamic background.
-        controller.dynamicBackground = true
-        
-
-        return controller
-    }()
+    lazy var imageViewController = MediaViewController(items: self.items,
+                                                       startingItem: self.startingItem,
+                                                       message: self.message)
 
     override func toPresentable() -> PresentableCoordinator<Void>.DismissableVC {
         return self.imageViewController
--- iOS/Flows/Media/MediaViewController.swift
+++ iOS/Flows/Media/MediaViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Lightbox
+import Lottie
 
 class MediaViewController: LightboxController, Dismissable, TransitionableViewController {
 
@@ -28,6 +29,72 @@ class MediaViewController: LightboxController, Dismissable, TransitionableViewCo
     }
 
     var dismissHandlers: [DismissHandler] = []
+    
+    let message: Messageable
+    
+    init(items: [MediaItem],
+         startingItem: MediaItem?,
+         message: Messageable) {
+        
+        self.message = message
+        
+        let images: [LightboxImage]
+        images = items.compactMap({ item in
+            guard let url = item.url else { return nil }
+            switch item.type {
+            case .photo:
+                return LightboxImage(imageURL: url, text: message.kind.text)
+            case .video:
+                guard let imageURL = item.previewURL else { return nil }
+                return LightboxImage(imageURL: imageURL, text: message.kind.text, videoURL: item.url)
+            }
+        })
+
+        var startIndex: Int = 0
+        if let start = startingItem {
+            for (index, item) in items.enumerated() {
+                if item.url == start.url {
+                    startIndex = index
+                    break
+                }
+            }
+        }
+        
+        LightboxConfig.PageIndicator.textAttributes = [.font: FontType.xtraSmall.font,
+                                                       .foregroundColor: ThemeColor.white.color.withAlphaComponent(0.2)]
+        
+        LightboxConfig.InfoLabel.textAttributes = [.font: FontType.small.font,
+                                                   .foregroundColor: ThemeColor.white.color]
+        
+        LightboxConfig.CloseButton.text = ""
+        LightboxConfig.CloseButton.image = UIImage(systemName: "xmark")
+        LightboxConfig.CloseButton.size = CGSize(width: 20, height: 18)
+        
+        let animationView = AnimationView.with(animation: .loading)
+        animationView.loopMode = .loop
+        animationView.play()
+        animationView.squaredSize = 18
+        
+        LightboxConfig.makeLoadingIndicator = {
+            animationView
+        }
+
+        super.init(images: images, startIndex: startIndex)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.headerView.closeButton.tintColor = ThemeColor.white.color
+        self.headerView.closeButton.imageView?.contentMode = .scaleAspectFit
+
+        // Use dynamic background.
+        self.dynamicBackground = true
+    }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
