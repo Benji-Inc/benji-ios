247a854ead2d2e2e2ceca9bb919b972699de756a
Benji <benji@jibber.wtf>
2022-06-21 14:12:49 -0700

    adding empty state to the notice collection view.

--- iOS/Flows/Home/Notices/NoticesCollectionView.swift
+++ iOS/Flows/Home/Notices/NoticesCollectionView.swift
@@ -7,8 +7,13 @@
 //
 
 import Foundation
+import Combine 
 
 class NoticesCollectionView: CollectionView {
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    let label = ThemeLabel(font: .medium)
     
     init() {
         super.init(layout: NoticesCollectionViewLayout())
@@ -23,7 +28,37 @@ class NoticesCollectionView: CollectionView {
 
     }
     
+    deinit {
+        self.cancellables.forEach { cancellable in
+            cancellable.cancel()
+        }
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func initializeViews() {
+        super.initializeViews()
+        
+        self.backView.addSubview(self.label)
+        self.label.textAlignment = .center
+        self.label.setText("You are all caught up! 😄")
+        self.label.alpha = 0
+        
+        NoticeStore.shared.$notices.mainSink { [unowned self] notices in
+            Task {
+                await UIView.awaitAnimation(with: .fast, delay: 0.25, animations: { [unowned self] in
+                    self.label.alpha = notices.count == 0 ? 1.0 : 0.0
+                })
+            }
+        }.store(in: &self.cancellables)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.label.setSize(withWidth: Theme.getPaddedWidth(with: self.width))
+        self.label.centerOnXAndY()
     }
 }
