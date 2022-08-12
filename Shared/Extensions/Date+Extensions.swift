ebf2042139af8a35e61007542c7f973f417645ba
Benji <benji@jibber.wtf>
2022-08-12 10:54:11 -0700

    moving extension

--- Shared/Extensions/Date+Extensions.swift
+++ Shared/Extensions/Date+Extensions.swift
@@ -316,3 +316,13 @@ extension Date {
     }
 }
 
+extension Calendar {
+    func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
+        let fromDate = startOfDay(for: from) // <1>
+        let toDate = startOfDay(for: to) // <2>
+        let numberOfDays = dateComponents([.day], from: fromDate, to: toDate) // <3>
+        
+        return numberOfDays.day!
+    }
+}
+
